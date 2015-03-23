af657ea1266b8f0dfe9a3333a1fd8c402b509658
Benoit Pierre <benoit.pierre@gmail.com>
2015-03-23 04:23:01 +0100

    file handler: move (most) UI code out to main
    It would be nice to be able to add tests for the file handler, but
    dependencies on the UI make it impossible. This improve the situation.

--- mcomix/main.py
+++ mcomix/main.py
@@ -9,6 +9,7 @@ import gobject
 from mcomix import constants
 from mcomix import cursor_handler
 from mcomix import i18n
+from mcomix import icons
 from mcomix import enhance_backend
 from mcomix import event
 from mcomix import file_handler
@@ -423,6 +424,7 @@ class MainWindow(gtk.Window):
             if self.is_manga_mode:
                 resolutions = tuple(reversed(resolutions))
             self.statusbar.set_resolution(resolutions)
+            self.statusbar.update()
 
             smartbg = prefs['smart bg']
             smartthumbbg = prefs['smart thumb bg'] and prefs['show thumbnails']
@@ -467,7 +469,6 @@ class MainWindow(gtk.Window):
                 self.images[i].hide()
             self._show_scrollbars([False] * len(self._scroll))
 
-        self._update_page_information()
         self._waiting_for_redraw = False
 
         return False
@@ -544,10 +545,16 @@ class MainWindow(gtk.Window):
 
     def _on_file_opened(self):
         self.uimanager.set_sensitivities()
+        number, count = self.filehandler.get_file_number()
+        self.statusbar.set_file_number(number, count)
+        self.statusbar.update()
 
     def _on_file_closed(self):
+        self.clear()
+        self.thumbnailsidebar.hide()
         self.thumbnailsidebar.clear()
         self.uimanager.set_sensitivities()
+        self.set_icon_list(*icons.mcomix_icons())
 
     def new_page(self, at_bottom=False):
         """Draw a *new* page correctly (as opposed to redrawing the same
@@ -570,7 +577,8 @@ class MainWindow(gtk.Window):
     @callback.Callback
     def page_changed(self):
         """ Called on page change. """
-        pass
+        self.thumbnailsidebar.load_thumbnails()
+        self._update_page_information()
 
     def set_page(self, num, at_bottom=False):
         if num == self.imagehandler.get_current_page():
@@ -680,6 +688,7 @@ class MainWindow(gtk.Window):
 
     def change_double_page(self, toggleaction):
         prefs['default double page'] = toggleaction.get_active()
+        self._update_page_information()
         self.draw_image()
 
     def change_manga_mode(self, toggleaction):
